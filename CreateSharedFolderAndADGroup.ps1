3f22d07bad8ef89f2f4a661556da2c31395156e8
Jens <50098214+jero02@users.noreply.github.com>
2019-04-29 11:40:30 +0200

    Update CreateSharedFolderAndADGroup.ps1
    Translation to english.

--- CreateSharedFolderAndADGroup.ps1
+++ CreateSharedFolderAndADGroup.ps1
@@ -1,10 +1,12 @@
-﻿##############################################################################################################
+##############################################################################################################
     <#  .Description
+    Change OU path and -server to match your environment. 
+
     1. Create folder, inheritance disabled. 
     2. Create AD group
     3. Add users to the group (optional)
 
-    After folder and group is creates, access rights are set for the group (read+write).                       
+    After folder and group is created, access rights are set for the group (read+write).                       
     #>   
 ##############################################################################################################
 
@@ -12,8 +14,11 @@
 function setFolderAccessRights {
 
     #20 sec paus to avoid IdentityNotMappedException
-    "Väntar 20 sekunder innan vi tilldelar gruppen skrivrättighet på mappen (annars hinner den inte hitta gruppen).."
+    "Wait 20 seconds before applying access rights to avoid problems.."
     Start-Sleep -Seconds 20
+
+    #for info about FileSystemRights Enum go to https://docs.microsoft.com/en-us/dotnet/api/system.security.accesscontrol.filesystemrights?view=netframework-4.8 
+
     $readWrite = [System.Security.AccessControl.FileSystemRights]4,2,64,32,1,131209,131241,128,8,131072,1048576,278,256,16
     #Inheritance
     $inheritanceFlag = [System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
@@ -30,53 +35,53 @@ function setFolderAccessRights {
     $objACL.AddAccessRule($accessControlEntry)
     Set-ACL $newFolderFull $objACL
 
-    Write-Host -ForegroundColor White -BackgroundColor DarkGreen "Skapat mapp $newFolderFull och grupp $groupName samt tilldelat behörighet, kontrollera att mapp+grupp+behörigheter blivit korrekta. Avslutar."
-    Start-Sleep -Seconds 7
+    Write-Host -ForegroundColor White -BackgroundColor DarkGreen "Folder $newFolderFull and group $groupName created with read-write access, always double check folder/group/rights has been created as expected. Quit in 10 sec.."
+    Start-Sleep -Seconds 15
 }
 
     #Reminder to open powershell as admin before starting script
-    Write-Host -ForegroundColor White -BackgroundColor Red "Öppna powershell som administratör innan du startar detta skript."
+    Write-Host -ForegroundColor White -BackgroundColor Red "Open powershell as Administrator before running this script."
   
         #ask for path
-        $path = Read-Host "Fyll i sökväg där mappen ska ligga t.ex. \\server\Shared\Folder"
-        $newFolderName = Read-Host "Skriv mappens namn"
+        $path = Read-Host "Path where the new folder will be created, example: \\server\Shared\Folder"
+        $newFolderName = Read-Host "New folder name"
         $newFolderFull = $path + "\" + $newFolderName
         $folderExists = Test-Path $newFolderFull
         $userNameADM = "admin-"+$env:USERNAME.Split("-")[-1]
     
     #check if path exists
     if ($folderExists -eq $true)
-        {"$newfolderFull verkar redan finnas, kontrollera sökvägen innan du kör skriptet igen, avslutar."; Start-Sleep -Seconds 10 ; break}
+        {"$newfolderFull already exists, verify the path/new folder name before running script, quitting.."; Start-Sleep -Seconds 10 ; break}
     
         #display new path, ask for confirmation
-        else {"Den nya mappen kommer bli: $newFolderFull"; $confirm = Read-Host "Stämmer sökvägen, gå vidare? J/N"}
+        else {"New folder will be: $newFolderFull"; $confirm = Read-Host "Is the new path correct, continue? Y/N"}
 
-    if ($confirm -ne "J") 
-        {'Kör skriptet igen när du är helt säker på sökväg/mappnamn, avslutar.'; Start-Sleep -Seconds 10 ; break}
+    if ($confirm -ne "Y") 
+        {'Quitting script..'; Start-Sleep -Seconds 10 ; break}
 
     #confirmed, disable inheritance
-    if ($confirm -eq "J" -and $folderExists -eq $false) 
-        {"Skapar mapp.."; New-Item $newFolderFull -ItemType Directory
-        "Inaktiverar ärvda behörigheter.."
+    if ($confirm -eq "Y" -and $folderExists -eq $false) 
+        {"Creating folder.."; New-Item $newFolderFull -ItemType Directory
+        "Disabling inheritance.."
         icacls $newFolderFull /inheritance:d
         
         #ask for group             
-        $groupName = Read-Host "Fyll i vad behörighetsgruppen ska heta, när du trycker enter får du fylla i lösenord för adm-kontot och sen skapas gruppen"
+        $groupName = Read-Host "Enter new group name and press enter, then log in with admin account to create the group"
         #login with admin account
-        $cred = Get-Credential -Message "Logga in med adm-konto för att skapa AD-grupp" -UserName $userNameADM
+        $cred = Get-Credential -Message "Log in with admin account to create group" -UserName $userNameADM
         
         #create group and set description and info
-        New-AdGroup -Server "DOMAIN.local" $groupName -samAccountName $groupName -GroupScope Global -path "OU=insertOUhere,OU=insertOUhere,OU=insertOUhere,DC=insertOUhere,DC=local" -Description $newFolderFull -OtherAttributes @{info="Skrivrättighet på $newFolderFull"} -Credential $cred}
+        New-AdGroup -Server "DOMAIN?.local" $groupName -samAccountName $groupName -GroupScope Global -path "OU=insertOUhere,OU=insertOUhere,DC=insertDomain,DC=local" -Description $newFolderFull -OtherAttributes @{info="Read+Write access on folder $newFolderFull"} -Credential $cred}
     
     #if group creation failed, quit    
     if ($? -eq $false) 
-        {'Misslyckades att skapa gruppen i AD, skapa grupp och sätt behörigheterna manuellt på mappen om det behövs, avslutar.'; break}
+        {'Failed to create AD group, handle access rights manually, quitting..'; Start-Sleep -Seconds 10 ; break}
     
-        else {$addMembers = Read-Host "Vill du lägga till medlemmar i $groupName (J/N)?"}
+        else {$addMembers = Read-Host "Add members to $groupName (Y/N)?"}
    
     #add members to group?
-    if ($addMembers -eq "J") {try {'Fyll i användarnamn ett åt gången, när du är klar tryck enter en gång utan att fylla i användarnamn för att gå vidare'
-        Add-ADGroupMember -Server "DOMAIN.local" -Identity $groupName -Credential $cred} catch {Write-Host -ForegroundColor White -BackgroundColor Red "Något blev fel när medlemmarna skulle läggas till i gruppen."}
+    if ($addMembers -eq "Y") {try {'Enter username one at a time, when finished press enter without entering username'
+        Add-ADGroupMember -Server "DOMAIN?.local" -Identity $groupName -Credential $cred} catch {Write-Host -ForegroundColor White -BackgroundColor Red "Something went wrong when adding users to the group."}
         
         #set rights
         setFolderAccessRights; break}
