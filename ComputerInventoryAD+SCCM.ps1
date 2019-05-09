7e8c517f52ce165f2716ea64ee40062251d22df6
Jens <50098214+jero02@users.noreply.github.com>
2019-05-09 19:35:08 +0200

    Update ComputerInventoryAD+SCCM.ps1

--- ComputerInventoryAD+SCCM.ps1
+++ ComputerInventoryAD+SCCM.ps1
@@ -1,13 +1,13 @@
-﻿<# .Description
+<# .Description
 
-    Search for computers that hasn't logged in for x days.
+    Search for computers that hasn't logged in for X days.
 
     1. Import AD and SCCM
-    2. Search in AD for computers
-    3. Search in SCCM for computers
-    4. Search in AD for last logged on users
+    2. Search AD for computers
+    3. Search SCCM for computers
+    4. Search AD for last logged on users
 
-    Result: pscustomobject containing computer info/lastlogontime etc and info about users
+    Result: pscustomobject containing computer info/lastlogontime/user info
 #>
 
 Import-Module ActiveDirectory
@@ -20,18 +20,24 @@ $sccmPath2 = Test-Path "C:\Program Files (x86)\Microsoft Configuration Manager\A
         } elseif ($sccmPath2) {Set-Location "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin"
         
         } else {Write-Host -ForegroundColor White -BackgroundColor Red 'Failed to import sccm module, quitting.';Start-Sleep -Seconds 10;break}    
-    
+
+#Import SCCM module and map session to SCCM site
 Import-Module .\ConfigurationManager.psd1
 New-PSDrive -Name [Site] -PSProvider "AdminUI.PS.Provider\CMSite" -Root "sccmserver.domain.local" -Description "SCCM Site"
-Set-Location Site:
-    
+Set-Location CMSiteHere:
+
+#Start script do loop
 do {
-[string]$ouInput = Read-Host "Which OU do you want to search?"[int]$daysInput = Read-Host "Days since the computers last logged in to AD"$dateResult = (Get-Date).AddDays("-"+$daysInput)
+
+[string]$ouInput = Read-Host "Which OU do you want to search?"
+[int]$daysInput = Read-Host "Days since the computers last logged in to AD"
+$dateResult = (Get-Date).AddDays("-"+$daysInput)
 $date = (get-date).ToString("yyyy-MM-dd")
-$computers = Get-ADComputer -Server "domain.local" -Filter 'LastLogonDate -lt $dateResult' -SearchBase "OU=Datorer,OU=$ouInput,OU=Adm,DC=domain,DC=local" -Properties CanonicalName,Enabled,LastLogonDate,WhenCreated
+$computers = Get-ADComputer -Server "domain.local" -Filter 'LastLogonDate -lt $dateResult' -SearchBase "OU=Computers,OU=$ouInput,OU=ou,DC=domain,DC=local" -Properties CanonicalName,Enabled,LastLogonDate,WhenCreated
 
  
-#to do: switch CM adlastlogontime for AD lastlogondate
+#to do: switch CM adlastlogontime for AD lastlogondate (adlastlogondate dont always exist on CMdevice object)
+#Loop to search for the computers in SCCM and build pscustomobject
 $Data = $computers.name | foreach {
         
 $Name = $_; $CMDevice = Get-CMDevice -Name $_
@@ -46,7 +52,8 @@ $Name = $_; $CMDevice = Get-CMDevice -Name $_
             [pscustomobject]@{Name=$Name;LastLoggedIn=$CMDevice.ADLastLogonTime;LastActive=$CMDevice.LastActiveTime;ADSiteName=$CMDevice.ADSiteName;LastLogonUser=$CMDevice.LastLogonUser}    
     }
 }               
-                                                                            
+
+#Loop to search for AD users and build the final result pscustomobject
 $DataResult = $Data | foreach {
 
 $ADuser = try {Get-ADUser -Server "domain.local" -Identity $_.LastLogonUser -Properties * | Select-Object displayName,UserPrincipalName,MobilePhone,telephoneNumber,Title,Manager} 
@@ -71,7 +78,7 @@ Write-Host -ForegroundColor White -BackgroundColor DarkGreen "Found $(@($DataRes
 Write-Host "1. Save result in a csv file ( $("$env:USERPROFILE\$ouInput$date.csv") )"
 Write-Host "2. Show result in console (might not see all columns)"
 $resultChoice = Read-Host
-                
+             
     switch ($resultChoice) {
     1 {$DataResult | sort "LastActive" | Export-Csv -Encoding UTF8 -Delimiter ";" -NoTypeInformation -NoClobber -Path $env:USERPROFILE\$ouInput$date.csv;
          Write-Host "Result saved in $env:USERPROFILE\$ouInput$date.csv"}
